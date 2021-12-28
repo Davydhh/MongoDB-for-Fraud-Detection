53174b7882c755d492a4ee4e226caddf6cf3dba4
RikJux <60518260+RikJux@users.noreply.github.com>
2021-12-28 17:59:21 +0100

    QUERY D) choice of product_kind and selection of day_period

--- query.py
+++ query.py
@@ -1,4 +1,5 @@
 import logging
+import random
 from ingestion import db
 from datetime import datetime
 
@@ -64,4 +65,26 @@ def run_query_a():
     print("Performance about query: {} milliseconds\n".format(performance_result.get("stages")[0].get("$cursor").get(
         "executionStats").get("executionTimeMillis")))
 
+def run_query_c():
+    logging.info("Running query c")
+    pass
 
+def run_query_d(product_kind= ["high-tech", "food", "clothing", "consumable", "other"],\
+    day_period= [{"name":"night", "start":"00:00:00"}, {"name":"morning", "start":"06:00:00"}, \
+        {"name":"afternoon", "start":"12:00:00"}, {"name":"evening", "start":"18:00:00"}]):
+    logging.info("Running query d")
+    #step one: extend the model
+    def select_product(product_kind=product_kind):
+        return random.choice(product_kind)
+
+    def select_period(time, period=day_period):
+        #suppose period ordered by 'start'
+        period_label = "null"
+        for p in period:
+            if datetime.strptime(time, "%H:%M:%S") >= datetime.strptime(p.get('start'), "%H:%M:%S"):
+                period_label = p.get('name')
+            else:
+                break
+        return period_label
+    #step two: buying_friends
+    return select_period()
