ee35ad4810f4392c756cff1ecedf995d80342fdf
Davide Cazzetta <davidepro@hotmail.it>
2022-01-07 16:21:36 +0100

    fix(ingestion): store in Mongo only the newest transactions dataset

--- ingestion.py
+++ ingestion.py
@@ -1,9 +1,8 @@
-import pymongo
 import json
-import os
 import logging
+import os
 import time
-
+import pymongo
 
 # client = pymongo.MongoClient("mongodb+srv://Davide:davide@cluster0.ovl1s.mongodb.net/projectDB?retryWrites"
 #                              "=true&w=majority")
@@ -13,20 +12,26 @@ db = client.projectDB
 
 
 def load_datasets():
+    logging.info("Storing datasets")
     start_time = time.time()
     path = "./datasets/"
     with open("{}customers.json".format(path)) as file:
         if db.customers.count() == 0:
             db.customers.insert_many(json.load(file))
+            logging.info("Customers dataset loaded")
 
     with open("{}terminals.json".format(path)) as file:
         if db.terminals.count() == 0:
             db.terminals.insert_many(json.load(file))
+            logging.info("Terminals dataset loaded")
 
-    for filename in os.listdir(path):
-        if filename.startswith("transactions"):
-            with open(os.path.join(path, filename)) as file:
-                db.transactions.insert_many(json.load(file))
+    transactions_files = [f for f in os.listdir(path) if f.startswith("transactions")]
+
+    latest_file = max(transactions_files)
+
+    with open(os.path.join(path, latest_file)) as file:
+        db.transactions.insert_many(json.load(file))
+        logging.info("{} dataset loaded".format(latest_file))
 
     logging.info("Datasets stored on MongoDB in {} seconds".format(time.time()-start_time))
 
