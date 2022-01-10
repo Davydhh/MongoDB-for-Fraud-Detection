e1e1b8811bd247ef71531aaeccbc5c51f5a59ec1
RikJux <60518260+RikJux@users.noreply.github.com>
2022-01-10 16:06:21 +0100

    size of data reduced

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from datasets_generator import DatasetsGenerator
 from ingestion import load_datasets
 
 if __name__ == "__main__":
-    nb_days = 180
+    nb_days = 40
     for y in range(2018, 2021):
         datasets_generator = DatasetsGenerator("{}-04-01".format(y), nb_days)
         datasets_generator.run()
